1e54744bf3f5cc3a0deb5402b5c729950400c351
Bruce <bruceplacathose@gmail.com>
2026-03-19 15:27:36 -0400

    added

--- services/product-service/db.js
+++ services/product-service/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: "mini-shopify-postgres",
+  user: "shopify",
+  password: "shopify",
+  database: "shopify_db",
+  port: 5432
 });
 
 module.exports = {
